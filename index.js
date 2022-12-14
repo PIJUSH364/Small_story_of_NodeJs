0484a359b5deb9da6178d7b2405f61ce4a9764ba
PIJUSH364 <praymondal@gmail.com>
2022-12-14 09:32:45 +0530

    content-type

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const express = require('express');
 require("dotenv").config()
 
 const app = express();
+app.use(express.json());// req body as a object return
+// app.use(express.raw());// req body as a buffer data send
 
+app.get('/', (req, res) => {
+    res.send('Express app running');
+})
 
-
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('post method calling');
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`app running on ${process.env.PORT}`);
